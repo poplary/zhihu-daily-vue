2c62153396a93ecb42a261949e31f3157448c26c
poplary <poplary_y@163.com>
2016-11-07 19:02:55 +0800

    修改配置

--- config/prod.env.js
+++ config/prod.env.js
@@ -1,3 +1,5 @@
 module.exports = {
-  NODE_ENV: '"production"'
+  NODE_ENV: '"production"',
+  API_URL: '"https://api.ipoplary.com/"',
+  LATEST_LIST: '"zhihu/latest"'
 }
--- config/test.env.js
+++ config/test.env.js
@@ -2,5 +2,7 @@ var merge = require('webpack-merge')
 var devEnv = require('./dev.env')
 
 module.exports = merge(devEnv, {
-  NODE_ENV: '"testing"'
+  NODE_ENV: '"testing"',
+  API_URL: '"https://api.ipoplary.com/"',
+  LATEST_LIST: '"zhihu/latest"'
 })
